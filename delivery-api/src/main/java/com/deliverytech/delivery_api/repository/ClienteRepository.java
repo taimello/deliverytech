3cb4920e4aa8e6886c918d78b0840367af1a72c6
Tainara Mello <tainarakarolina@gmail.com>
2025-10-08 10:49:52 -0300

    Implementação do clienteRepository

---
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/repository/ClienteRepository.java
@@ -0,0 +1,41 @@
+package com.deliverytech.delivery_api.repository;
+
+import com.deliverytech.delivery_api.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente,Long>{
+
+    //Buscar cliente por email
+    Optional<Cliente>findByEmail(String email);
+
+    //Verficiar se email já existe
+    boolean existsByEmail(String email);
+
+    //Buscar clientes ativods
+    List<Cliente>findByAtivoTrue();
+
+    //Buscar clientes por nome
+    List<Cliente>findByNomeContainingIgnoreCase(String nome);
+
+    //Buscar clientes por telefone
+    Optional<Cliente>findByTelefone(String telefone);
+
+    //Query customizada - clientes com pedidos
+    @Query("SELECT DISTINCT c FROM Cliente c JOIN c.pedidos p WHERE c.ativo = true")
+    List<Cliente>findClientesComPedidos();
+
+    //Query nativa - clientes por cidade
+    @Query(value = "SELEC * FROM clientes WHERE endereco LIKE %:cidade% AND ativo = true, nativeQuery = true")
+    List<Cliente>findByCidade(@Param("cidade")String cidade);
+
+    //Contar clientes ativos
+    @Query("SELECT COUNT(c) FROM Cliente c WHERE c.ativo = true")
+    Long countClientesAtivos();
+}
